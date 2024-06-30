ab25c75ab810ea968fde236c62a78e5ef8f34047
Nikhilesh796 <Nikhilesh796@gmail.com>
2024-06-30 13:30:23 +0530

    fourth commit

--- App.js
+++ App.js
@@ -6,11 +6,8 @@ const root = ReactDOM.createRoot(document.getElementById("root")); //to place th
 
 root.render(heading); //rendering the object into the dom
 
-*/
-import React from "react";
-import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, //creating a React element using core react.
     React.createElement("div", { id: "child" }, [
         React.createElement("h1", { id: "heading1" }, "React Nested elements"),
         React.createElement("h2", { id: "heading2" }, "React Nested elements two"),
@@ -19,3 +16,17 @@ const parent = React.createElement("div", { id: "parent" },
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
+
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React.createElement(after creating it,it becomes) => object => (when we render it,it becomes) HTML element.
+// This is how we create react element using JSX
+// The jsx code is transpiled before it reaches js Engine.Parcel will make the transpilation happen through babel.
+// JSX =>Babel transpiles it to React.createElement => Babel transpiles it to  Reactelement- JS object => HTML  element
+const heading = ( < h1 className = "Hello" >
+        Hello < /h1>);    
+
+        console.log(heading);
+        const root = ReactDOM.createRoot(document.getElementById("root")); root.render(heading);
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-React Project
+React Project:
+
+->Pacakges that you need to know abbout before starting with the project
+1.Parcel
+2.Babel
+3.Browsers-List
 
 #In our project we are using the parcel beacause
 -Dev build
@@ -18,11 +23,14 @@ React Project
 -Tree Shaking - remove unused code for us
 -different dev and production bundles
 
-#Use the commands
--npm install //to get all the node_modules
--npx parcel index.html // to intialize development build
--npx oarcel build index.html //to intialize production build
-
 #The production build is optimized for performance and size. It minifies and optimizes the code, removing unnecessary parts such as development warnings and error messages. This results in a smaller, faster application that is ready for deployment.
 
 #The development build for building and testing your app, and the production build for deploying a fast, optimized version of your app.
+
+#Use the commands
+-npm install //to get all the node_modules
+-npx parcel index.html // to intialize development build
+-npx parcel build index.html //to intialize production build
+-npm run start //to run the project
+(or)
+-npm start
